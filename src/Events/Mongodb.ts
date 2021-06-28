1cce3a8945d4833dbe285486fbc7db974ae1735b
Matteus <tolfx@tolfix.com>
2021-06-28 21:35:20 +0200

    New event < Mongodb.ts >

--- src/Events/Mongodb.ts
+++ src/Events/Mongodb.ts
@@ -1,1 +1,21 @@
-export {};
+import log from "../Lib/Logger";
+import mongoose from "mongoose";
+
+export default function MongodbEvent(db: mongoose.Connection)
+{
+    db.on('error', (error: any) => {
+        log.error(error)
+        log.error(`Closing application.. cannot run without mongoose.`, log.trace())
+        process.exit(1);
+    });
+
+    db.on('disconnected', () => {
+        log.error(`Got disconneted by mongoDB database..`, log.trace())
+        log.error(`Closing application.. cannot run without mongoose.`, log.trace())
+        process.exit(1);
+    })
+
+    db.once('open', () => {
+        log.info("Connected to database");
+    });
+}
