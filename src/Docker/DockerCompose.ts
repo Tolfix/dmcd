bb22c6b73a579adad6c381a46f9f219d4361d7a4
Matteus <tolfx@tolfix.com>
2021-06-29 22:56:18 +0200

    Modified < DockerCompose.ts >

--- src/Docker/DockerCompose.ts
+++ src/Docker/DockerCompose.ts
@@ -1,23 +1,21 @@
 import { stripIndent } from "common-tags"
 import { ICD } from "../Interfaces/CD";
 import cp from "child_process";
+import log from "../Lib/Logger";
 
 export function DockerCompose(dir: string): void
 {
+    
     const ls = cp.spawn('docker-compose', ['up', '-d'], {
         cwd: dir
     });
 
     ls.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
+            log.info(data)
     });
 
     ls.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-    });
-
-    ls.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
+            log.error(data)
     });
 
 }
