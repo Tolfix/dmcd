417946e3df1d8d5a148acb8b291ad6d3fa9eb005
Matteus <tolfx@tolfix.com>
2021-06-29 22:56:25 +0200

    Modified < Pull >

--- src/Docker/Pull.ts
+++ src/Docker/Pull.ts
@@ -14,12 +14,10 @@ export default function PullImage(image: string): Promise<Boolean>
             {
                 resolve(true);
             }
-            console.log(`stdout: ${data}`);
         });
 
         ls.stderr.on('data', (data) => {
             resolve(false)
-            console.error(`stderr: ${data}`);
         });
 
         ls.on('close', (code) => {
