7234ebd1901e7e51ac8ae56421a3eab10b23e85f
Matteus <tolfx@tolfix.com>
2021-06-29 21:59:41 +0200

    Added new config < DockerDir >

--- src/Config.ts
+++ src/Config.ts
@@ -4,3 +4,4 @@ export const Web_Title = process.env.TITLE ?? "DMCD";
 export const PORT = 56251;
 export const Session_Secret = process.env.SESSION_SECRET ?? undefined;
 export const Domain = process.env.DOMAIN ?? undefined
+export const DockerDir = ((__dirname.replace("\\build", "")).replace("/build", ""))+"/Docker";
