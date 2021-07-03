c8fe613f9a7f03764a0f392bdb997ec95e8e5667
Matteus <tolfx@tolfix.com>
2021-07-03 17:03:57 +0200

    Added < DockerDown.ts >

---
+++ src/Docker/DockerDown.ts
@@ -0,0 +1,20 @@
+import docker from "docker-compose";
+import AW from "../Lib/Async";
+import log from "../Lib/Logger";
+
+export default function DockerRemove(dir: string): Promise<Boolean>
+{
+    return new Promise(async (resolve, reject) => {
+        const [DS, D_Error] = await AW(docker.down({
+            cwd: dir
+        }));
+
+        if(D_Error)
+        {
+            log.error(`${DS?.err.trim()}`);
+            resolve(false);
+        }
+
+        resolve(true);
+    });
+}
