d6871137bf91393b28de169ecd6614b513c43416
Matteus <tolfx@tolfix.com>
2021-07-14 22:16:28 +0200

    Modified < Pull.ts >

--- src/Docker/Pull.ts
+++ src/Docker/Pull.ts
@@ -1,8 +1,10 @@
 import docker from "docker-compose";
 import AW from "../Lib/Async";
+import { getCDSocketLogs } from "../Lib/CDSocket";
 import log from "../Lib/Logger";
+import SOCKET from "../Server";
 
-export default function PullImage(dir: string): Promise<Boolean>
+export default function PullImage(dir: string, cdName?: string): Promise<Boolean>
 {
     return new Promise(async (resolve, reject) => {
         const [Image, I_Error] = await AW(docker.pullAll({
@@ -11,10 +13,14 @@ export default function PullImage(dir: string): Promise<Boolean>
 
         if(I_Error)
         {
+            if(cdName)
+                SOCKET.emit(getCDSocketLogs(cdName), `Failed to pull image`);
             log.error(`Unable to pull image`);
             return resolve(false);
         }
 
+        if(cdName)
+            SOCKET.emit(getCDSocketLogs(cdName), `Succesfully pulled image`);
         return resolve(true);
     });
 }
