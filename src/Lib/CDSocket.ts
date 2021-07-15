54329f818f56a0176f387e5adfe4298d8c63c614
Tolfix <matteus12122@gmail.com>
2021-07-15 10:46:57 +0200

    More dynamic in < CDSocket.ts >

--- src/Lib/CDSocket.ts
+++ src/Lib/CDSocket.ts
@@ -1,17 +1,19 @@
+import { Active, Building, Failed, Statues } from "../Types/Statues";
+
 //@ts-nocheck
 export function getCDSocketFail<Name extends string>(name: Name): `cd-${Name}-fail`
 {
-    return `cd-${name}-fail`;
+    return `cd-${name}-${Failed}`;
 }
 
 export function getCDSocketBuild<Name extends string>(name: Name): `cd-${Name}-building`
 {
-    return `cd-${name}-building`;
+    return `cd-${name}-${Building}`;
 }
 
 export function getCDSocketActive<Name extends string>(name: Name): `cd-${Name}-active`
 {
-    return `cd-${name}-active`;
+    return `cd-${name}-${Active}`;
 }
 
 export function getCDSocketLogs<Name extends string>(name: Name): `cd-${Name}-logs`
