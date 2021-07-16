c5a9ab65224426141f8a837a4cd9dce8d288b86d
Tolfix <matteus12122@gmail.com>
2021-07-16 15:26:35 +0200

    Small changes

--- src/Server.ts
+++ src/Server.ts
@@ -81,7 +81,7 @@ server.use((req, res, next) => {
     next();
 });
 
-const sv = server.listen(PORT, () => log.info(`Server listing on http://localhost:${PORT}/`));
+const sv = server.listen(PORT, () => log.info(`Server listing on ${HTTPS}://${Domain}${Domain === "localhost" ? ":"+PORT : ""}/`));
 const io = (new SocketIo(sv)).io;
 
 new MainRouter(server);
