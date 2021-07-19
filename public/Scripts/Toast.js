7fb74c27cac6bea3438934d31cf888c2a6a595d2
Tolfix <matteus12122@gmail.com>
2021-07-19 10:02:11 +0200

    Toast not removing itself #31 fixed

--- public/Scripts/Toast.js
+++ public/Scripts/Toast.js
@@ -1,13 +1,15 @@
+let toastCount = 0;
+
 function Toast(msg, status, title)
 {
     let TOAST = document.createElement("div");
 
     TOAST.innerHTML = `
-    <div class="toast border-${status} border-top" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
+    <div id="toast-${toastCount}" data-bs-autohide="false" class="toast border-${status} border-top" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
         <div class="toast-header">
             <img src="https://cdn.tolfix.com/images/TX-Small.png" width="28" class="rounded mr-2" alt="...">
             <strong class="me-auto">${title}</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            <button id="toast-button-${toastCount}" type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">
             ${msg}
@@ -19,4 +21,10 @@ function Toast(msg, status, title)
     $(document).ready(function(){
         $('.toast').toast('show');
     });
+    let tempCount = toastCount;
+    document.querySelector(`#toast-button-${toastCount}`).addEventListener("click", (msg) => {
+        document.querySelector(`#toast-${tempCount}`).remove()
+    })
+
+    toastCount++
 }
